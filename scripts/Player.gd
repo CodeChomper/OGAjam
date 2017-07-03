77d5b81db2dc630cfa862b8cb260886cd5788308
CodeChomper <GIT@CodeChomper.com>
2017-07-03 05:51:23 -0700

    Movement and Animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,14 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 onready var rayFloor = get_node("rayFloor")
 onready var onGround = false
 onready var vel = Vector2(0,0)
-onready var WALK_SPEED = 1000
-onready var JUMP_SPEED = 30
+onready var WALK_SPEED = 10
+onready var JUMP_SPEED = 70
+onready var GRAVITY = 300
+onready var facingLeft = false
+onready var state = "idle"
+onready var anim = get_node("AnimationPlayer")
 
 func _ready():
 	set_fixed_process(true)
@@ -15,19 +19,50 @@ func _fixed_process(delta):
 	var right = Input.is_action_pressed("p_right")
 	var up = Input.is_action_pressed("p_up")
 	
-	# Handle movement
-	vel.y = self.get_linear_velocity().y
-	vel.x *= 0.9
-	if(left and not right):
-		vel.x -= WALK_SPEED * delta
-	if(right and not left):
-		vel.x += WALK_SPEED * delta
-	if up and rayFloor.is_colliding():
-		vel.y -= JUMP_SPEED
-	self.set_linear_velocity(vel)
+	onGround = rayFloor.is_colliding()
 	
-	# Check if on ground
-	if rayFloor.is_colliding():
-		onGround = true
-	else:
-		onGround = false
+	# Handle Movement
+	gravity(delta)
+	if left:
+		flip(true)
+		vel.x -= WALK_SPEED
+	if right:
+		flip(false)
+		vel.x += WALK_SPEED
+	vel.x *= 0.9
+	
+	if (right or left) and onGround:
+		state = "Walk"
+	
+	if onGround and abs(vel.x) < 3:
+		state = "idle"
+	
+	var motion = vel * delta
+	move(motion)
+	
+	if is_colliding():
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		vel = n.slide(vel)
+		move(motion)
+	
+	# Handle Jump
+	if rayFloor.is_colliding() and up:
+		vel.y -= JUMP_SPEED
+	
+	if !onGround:
+		state = "jump"
+	
+	var animation = anim.get_current_animation()
+	if animation != state:
+		anim.play(state)
+
+
+func gravity(delta):
+	vel.y += GRAVITY * delta
+
+func flip(left):
+	var s = get_scale()
+	if left: s.x *= -1
+	else: s.x *= 1
+	set_scale(s)
